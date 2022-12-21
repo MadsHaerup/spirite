109184fe3ca3a1303927abcff37d86ec94b10d4d
Mads Hærup <madshaerup@outlook.com>
2022-12-21 15:14:45 +0100

    dark mode logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Image } from 'react-native';
+import { ThemeContext } from '../../context/context';
 
 export default function Logo() {
+	const { isThemeDark } = useContext(ThemeContext);
 	return (
 		<View style={{ marginBottom: 20 }}>
-			<Image style={{ width: '100%', height: 100 }} source={require('../../../assets/images/logo.png')} />
+			<Image
+				style={{ width: '100%', height: 100 }}
+				source={
+					isThemeDark ? require('../../../assets/images/logo.png') : require('../../../assets/images/logo_dark.png')
+				}
+			/>
 		</View>
 	);
 }
